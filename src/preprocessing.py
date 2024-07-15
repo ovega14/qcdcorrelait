b28f545eb1bb9e1fc99671e043a67415df92c4e6
ovega141 <vega00087@gmail.com>
2024-07-15 11:25:36 +0200

    implemented prepare_data and other processing utilities in preproces.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,13 @@
 import h5py
 import numpy as np
+import torch
 
 import numpy.typing as npt
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 
+
+"""Utilities for processing correlator data."""
 NUM_TSRC: int = 24  # 24 source times for our datasets.
 
 
@@ -98,3 +101,194 @@ def normalize_1d(corr: npt.Array) -> Tuple[npt.Array, npt.NDArray]:
     normalized_corr = corr / np.mean(corr, axis=0)
 
     return normalized_corr, normalzation_factors
+
+
+def average_tsrc(corr: npt.NDArray) -> npt.NDArray:
+    """
+    Averages correlator data over the source times and orders by configuration.
+
+    Args:
+        numpy array of shape [Nt, Nconf, Ntsrc]
+    Returns:
+        numpy array of shape [Nconf, Nt]
+    """
+    return np.average(corr, axis=2).transpose()  # [nconf, tau]
+
+
+#===================================================================================================
+# MODEL INPUT PREPARATION
+#===================================================================================================
+def prepare_input(
+    corr_s: npt.NDArray,
+    corr_l: npt.NDArray,
+    train_ind_list: List[int],
+    bc_ind_list: List[int],
+    unlab_ind_list: List[int],
+    t_extent: int
+) -> Tuple[npt.NDArray, ...]:
+    """
+    Prepares input data from normalized strange and light correlators for supervised learning.
+
+    The major and secondary axes for "axis-0 of X" are configurations and source times, 
+    respectively. Similarly for "axis-0 of Y."
+
+    Args:
+        corr_s:
+        corr_l:
+        train_ind_list: List of source time indices for training data
+        bc_ind_list: List of source time indices for bias-correction data
+        unlab_ind_list: List of source time indices for unlabeled data
+        t_extent: The time extent
+    
+    Returns:
+        X_train: numpy NDArray shaped [n_train * num_configs, num_t_extents]
+        Y_train: numpy NDArray shaped [n_train * num_configs, ]
+        X_bc: numpy NDArray shaped [n_bc * num_configs, num_t_extents]
+        Y_bc: numpy NDArray shaped [n_bc * num_configs, ]
+        X_unlab: numpy NDArray shaped [n_unlab * num_configs, num_t_extents]
+        Y_unlab:
+        norm_factors_X:
+        norm_factors_Y:
+    """
+    num_tau = corr_s.shape[0]
+
+    # Retrieve and normalize strange correlator data
+    X_train = corr_s[:, :, train_ind_list]
+    X_bc = corr_s[:, :, bc_ind_list]
+    X_unlab = corr_s[:, :, unlab_ind_list]
+
+    X_train, norm_factors_X = normalize_3d(X_train)
+    X_bc = X_bc * norm_factors_X
+    X_unlab = X_unlab * norm_factors_X
+
+    # Retrieve and normalize (output) light correlator data
+    Y_train = corr_l[t_extent, :, train_ind_list]
+    Y_bc = corr_l[t_extent, :, bc_ind_list]
+    Y_unlab = corr_l[t_extent, :, unlab_ind_list]
+
+    norm_factors_Y = 1. / np.average(Y_train)
+    
+    Y_train = Y_train * norm_factors_Y
+    Y_bc = Y_bc * norm_factors_Y
+    Y_unlab = Y_unlab * norm_factors_Y
+
+    # Reshape all data
+    X_train = X_train.reshape((num_tau, -1)).transpose()
+    X_bc = X_bc.reshape((num_tau, -1)).transpose()
+    X_unlab = X_unlab.reshape((num_tau, -1)).transpose()
+
+    Y_train = Y_train.flatten()
+    Y_bc = Y_bc.flatten()
+    Y_unlab = Y_unlab.flatten()
+
+    norm_factors_X = norm_factors_X.flatten()  # [num_train * num_cfgs * num_tau]
+
+    return X_train, Y_train, X_bc, Y_bc, X_unlab, Y_unlab, norm_factors_X, norm_factors_Y
+
+
+def prepare_input_seq2seq(
+    corr_s: npt.NDArray,
+    corr_l: npt.NDArray,
+    train_ind_list: List[int],
+    bc_ind_list: List[int],
+    unlab_ind_list: List[int]
+) -> Tuple[npt.NDArray, ...]:
+    """
+    Prepares input data from normalized strange and light correlators for sequence to sequence
+    supervised learning.
+
+    The major and secondary axes for "axis-0 of X" are configurations and source times, 
+    respectively. Similarly for "axis-0 of Y."
+
+    Args:
+        corr_s:
+        corr_l:
+        train_ind_list: List of source time indices for training data
+        bc_ind_list: List of source time indices for bias-correction data
+        unlab_ind_list: List of source time indices for unlabeled data
+        t_extent: The time extent
+    
+    Returns:
+        X_train: numpy NDArray shaped [n_train * num_configs, num_t_extents]
+        Y_train: numpy NDArray shaped [n_train * num_configs, ]
+        X_bc: numpy NDArray shaped [n_bc * num_configs, num_t_extents]
+        Y_bc: numpy NDArray shaped [n_bc * num_configs, ]
+        X_unlab: numpy NDArray shaped [n_unlab * num_configs, num_t_extents]
+        Y_unlab:
+        norm_factors_X:
+        norm_factors_Y:
+    """
+    num_tau = corr_s.shape[0]
+
+    X_train = corr_s[:, :, train_ind_list]
+    X_bc = corr_s[:, :, bc_ind_list]
+    X_unlab = corr_s[:, :, unlab_ind_list]
+
+    # normalization
+    X_train, norm_factors_X = normalize_3d(X_train)
+    X_bc *= norm_factors_X
+    X_unlab *= norm_factors_X
+
+    Y_train = corr_l[:, :, train_ind_list]
+    Y_bc = corr_l[:, :, bc_ind_list]
+    Y_unlab = corr_l[:, :, unlab_ind_list]
+
+    Y_train, norm_factors_Y = normalize_3d(Y_train)
+    Y_bc *= norm_factors_Y
+    Y_unlab *= norm_factors_Y
+
+    # reshaping
+    X_train = X_train.reshape((num_tau, -1)).transpose()
+    X_bc = X_bc.reshape((num_tau, -1)).transpose()
+    X_unlab = X_unlab.reshape((num_tau, -1)).transpose()
+
+    Y_train = Y_train.reshape((num_tau, -1)).transpose()
+    Y_bc = Y_bc.reshape((num_tau, -1)).transpose()
+    Y_unlab = Y_unlab.reshape((num_tau, -1)).transpose()
+
+    norm_factors_X = norm_factors_X.flatten()
+    norm_factors_Y = norm_factors_Y.flatten()
+
+    return X_train, Y_train, X_bc, Y_bc, X_unlab, Y_unlab, norm_factors_X, norm_factors_Y
+
+
+#===================================================================================================
+# INDEXING AND CONVERTING
+#===================================================================================================
+def tensor_data_by_ind_list(
+    raw_corr: npt.NDArray, 
+    ind_list: Optional[List[int]] = list(range(NUM_TSRC))
+) -> torch.Tensor:
+    """
+    Convets a 3d numpy array `raw_corr` to a 2d torch tensor (partially flattened according to
+    `ind_list`).
+
+    Args:
+        ind_list: List of integer indices for the time sources
+        raw_corr: 3d numpy array of shape [num_tau, num_cfgs, num_tsrc]
+
+    Returns:
+        Reshaped PyTorch tensor of `raw_corr` with source times filtered by `ind_list`, shaped as
+        [num_cfgs * len(ind_list), num_tau].
+    """
+    raw_corr = torch.from_numpy(raw_corr[:, :, ind_list]).double()
+    return torch.flatten(raw_corr, start_dim=1, end_dim=-1).T
+
+
+def tensor_data_to_np_data_3d(
+    tensor_corr: torch.Tensor,
+    num_cfgs: int,
+    num_taus: int
+) -> npt.NDArray:
+    """
+    Converts correlator data from a PyTorch tensor to a 3d numpy array.
+
+    Args:
+        tensor_corr: torch.Tensor shaped [num_cfgs * len(ind_list), num_tau]
+        num_configs: number of configurations in data
+
+    Returns:
+        reshaped numpy 3d array; [n_taus, n_configs, len(ind_list)]
+    """
+    np_corr = tensor_corr.T.numpy().reshape((num_taus, num_cfgs, -1))
+    return np_corr
