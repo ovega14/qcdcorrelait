73ae81d8f7439431ed4efe2e2b95be832cf5838a
ovega141 <vega00087@gmail.com>
2024-07-17 16:45:00 +0200

    implemented analyze and renamed stats to plot

--- src/analysis/analyze.py
+++ src/analysis/analyze.py
@@ -1,10 +1,145 @@
+import numpy as np
+import gvar as gv
+import numpy.typing as npt
+
+from .plot import plot_correlators, plot_relative_correlated_difference, plot_noise_to_signal, plot_normalized_noise_to_signal, plot_error_breakdown
+from ..inference.ratio_method import ratio_method
+from ..processing.preprocessing import tensor_to_avg_over_tsrc
+from ..fitting.fit import fit_corrs
+
+
 def analysis_pred(
-        corr_i, corr_o,
-        n_tau, n_configs, n_tsrc,
-        args,
-        dict_data,
-        dict_results,
-    ) -> None:
+    corr_i, 
+    corr_o,
+    num_tau: int, 
+    num_cfgs: int, 
+    num_tsrc: int,
+    dict_data: dict[str, npt.NDArray],
+    dict_results: dict[str, npt.NDArray],
+    *args,
+) -> None:
+    """
+    Performs the full analysis of the predicted correlator data, including plots and tables.
+
+    Args:
+        corr_i: 
+        corr_o:
+        num_tau: Number of Euclidean time extents used in fitting the correlator data
+        num_cfgs: Number of configurations in the original dataset
+        num_tsrc: Number of source times in the original dataset
+        dict_data: Dictionary of originally preprocessed correlator data
+        dict_results: Dictionary of resultant correlator data from inference
+    """
+    # COLLECT NECESSSARY DATA FROM INPUTS AND RESULTS ----------------------------------------------
+    n_corr_o_unlab_tensor = dict_data["n_corr_o_unlab_tensor"]
+    n_corr_o_bc_tensor = dict_data["n_corr_o_bc_tensor"]
+    corr_o_train_stds = dict_data["corr_o_train_stds"]
+    corr_o_train_means = dict_data["corr_o_train_means"]
+
+    corr_o_train_pred_tensor = dict_results["corr_o_train_pred_tensor"]
+    corr_o_unlab_pred_tensor = dict_results["corr_o_unlab_pred_tensor"]
+    corr_o_bc_pred_tensor = dict_results["corr_o_bc_pred_tensor"]
+    n_corr_o_unlab_pred_tensor = dict_results["n_corr_o_unlab_pred_tensor"]
+    n_corr_o_bc_pred_tensor = dict_results["n_corr_o_bc_pred_tensor"]
+
+    # Get ratio method data
+    if args.compare_ratio_method == 1:
+        ds_ratio_method = ratio_method(
+            corr_i=corr_i,
+            corr_o=corr_o,
+            lab_ind_list=args.train_ind_list+args.bc_ind_list,
+            modify=args.modify_ratio,
+        )
+
+    if args.compare_ml_ratio_method == 1:
+        corr_o_train_pred = corr_o_train_pred_tensor.T.reshape((num_tau, num_cfgs, -1)).numpy()
+        corr_o_bc_pred = corr_o_bc_pred_tensor.T.reshape((num_tau, num_cfgs, -1)).numpy()
+        corr_o_unlab_pred = corr_o_unlab_pred_tensor.T.reshape((num_tau, num_cfgs, -1)).numpy()
+        corr_o_pred = []
+        curr_ind_train = 0
+        curr_ind_bc = 0
+        curr_ind_unlab = 0
+        for i in range(num_tsrc):
+            if i in args.train_ind_list:
+                corr_o_pred.append(corr_o_train_pred[:, :, curr_ind_train])
+                curr_ind_train += 1
+            elif i in args.bc_ind_list:
+                corr_o_pred.append(corr_o_bc_pred[:, :, curr_ind_bc])
+                curr_ind_bc += 1
+            else:
+                corr_o_pred.append(corr_o_unlab_pred[:, :, curr_ind_unlab])
+                curr_ind_unlab += 1
+        corr_o_pred = np.array(corr_o_pred)
+        corr_o_pred = np.swapaxes(corr_o_pred, 0, 1)
+        corr_o_pred = np.swapaxes(corr_o_pred, 1, 2)
+        print("corr_o_pred.shape:", corr_o_pred.shape, )
+        print("corr_o.shape:", corr_o.shape, )
+        ds_ml_ratio_method = ratio_method(
+            corr_i = corr_o_pred,
+            corr_o = corr_o,
+            lab_ind_list = args.train_ind_list + args.bc_ind_list,
+            modify = args.modify_ratio,
+        )
+
+    # POST-PROCESS DATA ----------------------------------------------------------------------------
+    n_corr_o_unlab_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_unlab_tensor, num_tau, num_cfgs)
+    n_corr_o_unlab_pred_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_unlab_pred_tensor, num_tau, num_cfgs)
+
+    n_corr_o_bc_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_bc_tensor, num_tau, num_cfgs)
+    n_corr_o_bc_pred_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_bc_pred_tensor, num_tau, num_cfgs)
+
+    n_corr_o_uncorrected = n_corr_o_unlab_pred_vs_tau
+    n_corr_o_corrected = n_corr_o_uncorrected + n_corr_o_bc_vs_tau - n_corr_o_bc_pred_vs_tau
+    n_bias_correction_vs_tau = n_corr_o_bc_vs_tau - n_corr_o_bc_pred_vs_tau
+
+    corr_o_pred_corrected = n_corr_o_corrected * corr_o_train_stds[:, None] + corr_o_train_means[:, None]
+    corr_o_pred_uncorrected = n_corr_o_uncorrected * corr_o_train_stds[:, np.newaxis] + corr_o_train_means[:, np.newaxis]
+    corr_o_bias_correction = n_bias_correction_vs_tau * corr_o_train_stds[:, np.newaxis] + corr_o_train_means[:, np.newaxis]
+    corr_o_truth = np.average(corr_o, axis=-1)
+    corr_o_train_truth = np.average(corr_o[:, :, args.train_ind_list], axis=-1)
+    corr_o_labeled_truth = np.average(corr_o[:, :, args.train_ind_list + args.bc_ind_list], axis=-1)
+
+    # MAKE PLOTS -----------------------------------------------------------------------------------
+    plot_correlators(
+        num_tau = num_tau,
+        corr_o_truth = corr_o_truth,
+        corr_o_train_truth = corr_o_train_truth,
+        corr_o_pred_corrected = corr_o_pred_corrected,
+        corr_o_pred_uncorrected = corr_o_pred_uncorrected,
+        ds_ratio_method = ds_ratio_method,
+        ds_ml_ratio_method = ds_ml_ratio_method,
+        *args
+    )
+
+    plot_relative_correlated_difference(
+        n_corr_o_unlab_vs_tau, 
+        n_corr_o_unlab_pred_vs_tau,
+        n_corr_o_bc_vs_tau,
+        n_corr_o_bc_pred_vs_tau
+    )
+
+    plot_noise_to_signal(
+        num_tau = num_tau,
+        corr_o_truth = corr_o_truth,
+        corr_o_labeled_truth = corr_o_labeled_truth,
+        corr_o_pred_corrected = corr_o_pred_corrected,
+        corr_o_pred_uncorrected = corr_o_pred_uncorrected,
+        ds_ratio_method = ds_ratio_method,
+        ds_ml_ratio_method = ds_ml_ratio_method,
+        *args
+    )
+
+    plot_normalized_noise_to_signal(
+        num_tau = num_tau,
+        corr_o_truth = corr_o_truth,
+        corr_o_labeled_truth = corr_o_labeled_truth,
+        corr_o_pred_corrected = corr_o_pred_corrected,
+        corr_o_pred_uncorrected = corr_o_pred_uncorrected,
+        ds_ratio_method = ds_ratio_method,
+        ds_ml_ratio_method = ds_ml_ratio_method,
+        *args
+    )
+
     plot_error_breakdown(
         pred_corrected=corr_o_pred_corrected,
         pred_uncorrected=corr_o_pred_uncorrected,
@@ -13,6 +148,7 @@ def analysis_pred(
         truth=corr_o_truth,
     )
 
+    # WRITE RESULTS --------------------------------------------------------------------------------
     dict_orig_corrs = dict()
     print("corr_o_truth.shape", corr_o_truth.shape)
     corrs = [corr_o_truth, corr_o_pred_corrected, corr_o_pred_uncorrected]
@@ -23,8 +159,8 @@ def analysis_pred(
         dict_orig_corrs[name] = corr
 
     dict_opts = dict()
-    opt_keys = ['l', 'tp', 'tmin', 'tmax', 'ne', 'no', 'maxit', 'averages_tsrc']
-    opt_vals = [args.input_dataname + '_' + args.output_dataname, n_tau, 2, n_tau-2, 5, 5, 5_000, False]
+    opt_keys = ['filename', 'tp', 'tmin', 'tmax', 'ne', 'no', 'maxit', 'averages_tsrc']
+    opt_vals = [args.input_dataname + '_' + args.output_dataname, num_tau, 2, num_tau-2, 5, 5, 5_000, False]
     for key, val in zip(opt_keys, opt_vals):
         dict_opts[key] = val
     l = dict_opts.get('l')
@@ -55,7 +191,6 @@ def analysis_pred(
             print(string, file=f)
             print('=' * 120, file=f)
 
-    # print("ds_ratio_method =", ds_ratio_method)
     if args.compare_ratio_method == 1:
         dict_fits = fit_corrs(
             dict_corrs=None,
--- src/analysis/plot.py
+++ src/analysis/plot.py
@@ -1,12 +1,13 @@
 import numpy as np
+import gvar as gv
 from scipy.stats import sem
-import matplotlib
-import matplotlib.pyplot as plt
 
 import numpy.typing as npt
 
+import matplotlib
+import matplotlib.pyplot as plt
+
 from ..plotting import save_plot
-from ..processing.preprocessing import tensor_to_avg_over_tsrc
 
 
 #===================================================================================================
@@ -25,114 +26,78 @@ matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'  # for \tex
 #===================================================================================================
 # CORRELATORS
 #===================================================================================================
-def plot_correlators(*args) -> None:
+def plot_correlators(
+    num_tau: int,
+    corr_o_truth: npt.NDArray,
+    corr_o_train_truth: npt.NDArray,
+    corr_o_pred_corrected: npt.NDArray,
+    corr_o_pred_uncorrected: npt.NDArray,
+    ds_ratio_method: dict[str, npt.NDArray],
+    ds_ml_ratio_method: dict[str, npt.NDArray],
+    *args
+) -> None:
     """
     Plots correlator data along with predicted correlators on the same figure.
-    """
-    corr_o_pred_corrected = n_corr_o_corrected * corr_o_train_stds[:, None] + corr_o_train_means[:, None]
-    corr_o_pred_uncorrected = n_corr_o_uncorrected * corr_o_train_stds[:, np.newaxis] + corr_o_train_means[:, np.newaxis]
-    corr_o_bias_correction = n_bias_correction_vs_tau * corr_o_train_stds[:, np.newaxis] + corr_o_train_means[:, np.newaxis]
-    corr_o_truth = np.average(corr_o, axis=-1)
-    corr_o_train_truth = np.average(corr_o[:, :, args.train_ind_list], axis=-1)
-    corr_o_labeled_truth = np.average(corr_o[:, :, args.train_ind_list + args.bc_ind_list], axis=-1)
 
+    Args:
+        num_tau: Number of time extents being used in the fit
+        corr_o_pred_corrected: Bias-corrected correlator predicted by the ML model
+        corr_o_pred_uncorrected: Bias-uncorrected correlator predicted by the ML model
+        corr_o_truth: Truth-level correlator data
+        corr_o_train_truth: Subset of truth data reserved for training
+        ds_ratio_method: Dictionary of correlators output by ratio method
+        ds_ML_ratio_method: Dictionary of correlators output by ratio method combined with ML
+    """
     fig = plt.figure(figsize=(8., 6.))
 
     plt.errorbar(
-        x=np.arange(0, n_tau),
-        y=np.average(corr_o_truth, axis=-1),
-        yerr=sem(corr_o_truth, axis=-1),
-        c='b',
-        label='Truth',
+        x = np.arange(0, num_tau),
+        y = np.average(corr_o_truth, axis=-1),
+        yerr = sem(corr_o_truth, axis=-1),
+        c = 'b',
+        label = 'Truth',
     )
 
     plt.errorbar(
-        x=np.arange(0, n_tau),
-        y=np.average(corr_o_train_truth, axis=-1),
-        yerr=sem(corr_o_train_truth, axis=-1),
-        c='g',
-        label='Training set',
+        x = np.arange(0, num_tau),
+        y = np.average(corr_o_train_truth, axis=-1),
+        yerr = sem(corr_o_train_truth, axis=-1),
+        c = 'g',
+        label = 'Training set',
     )
 
     plt.errorbar(
-        x=np.arange(0, n_tau),
-        y=np.average(corr_o_pred_corrected, axis=-1),
-        yerr=sem(corr_o_pred_corrected, axis=-1),
-        c='r',
-        label='Pred Corrected',
+        x = np.arange(0, num_tau),
+        y = np.average(corr_o_pred_corrected, axis=-1),
+        yerr = sem(corr_o_pred_corrected, axis=-1),
+        c = 'r',
+        label = 'Pred Corrected',
     )
 
     plt.errorbar(
-        x=np.arange(0, n_tau),
-        y=np.average(corr_o_pred_uncorrected, axis=-1),
-        yerr=sem(corr_o_pred_uncorrected, axis=-1),
-        c='gold',
-        label='Pred Uncorrected',
+        x = np.arange(0, num_tau),
+        y = np.average(corr_o_pred_uncorrected, axis=-1),
+        yerr = sem(corr_o_pred_uncorrected, axis=-1),
+        c = 'gold',
+        label = 'Pred Uncorrected',
     )
 
+    # Optionally include ratio method results
     if args.compare_ratio_method == 1:
-        ds_ratio_method = ratio_method(
-            corr_i=corr_i,
-            corr_o=corr_o,
-            lab_ind_list=args.train_ind_list+args.bc_ind_list,
-            modify=args.modify_ratio,
-        )
-
-    if args.compare_ml_ratio_method == 1:
-        corr_o_train_pred = corr_o_train_pred_tensor.T.reshape((n_tau, n_configs, -1)).numpy()
-        corr_o_bc_pred = corr_o_bc_pred_tensor.T.reshape((n_tau, n_configs, -1)).numpy()
-        corr_o_unlab_pred = corr_o_unlab_pred_tensor.T.reshape((n_tau, n_configs, -1)).numpy()
-        # corr_o_train_pred = np.swapaxes(corr_o_train_pred, 0, 1)
-        # corr_o_bc_pred = np.swapaxes(corr_o_bc_pred, 0, 1)
-        # corr_o_unlab_pred = np.swapaxes(corr_o_unlab_pred, 0, 1)
-        corr_o_pred = []
-        curr_ind_train = 0
-        curr_ind_bc = 0
-        curr_ind_unlab = 0
-        for i in range(n_tsrc):
-            if i in args.train_ind_list:
-                corr_o_pred.append(corr_o_train_pred[:, :, curr_ind_train])
-                curr_ind_train += 1
-            elif i in args.bc_ind_list:
-                corr_o_pred.append(corr_o_bc_pred[:, :, curr_ind_bc])
-                curr_ind_bc += 1
-            else:
-                corr_o_pred.append(corr_o_unlab_pred[:, :, curr_ind_unlab])
-                curr_ind_unlab += 1
-        corr_o_pred = np.array(corr_o_pred)
-        corr_o_pred = np.swapaxes(corr_o_pred, 0, 1)
-        corr_o_pred = np.swapaxes(corr_o_pred, 1, 2)
-        print("corr_o_pred.shape:", corr_o_pred.shape, )
-        print("corr_o.shape:", corr_o.shape, )
-        ds_ml_ratio_method = ratio_method(
-            # corr_i=np.expand_dims(corr_o_pred_uncorrected, axis=-1),
-            # corr_i=corr_o_unlab_pred_tensor,
-            corr_i=corr_o_pred,
-            corr_o=corr_o,
-            # lab_ind_list=[0],
-            lab_ind_list=args.train_ind_list+args.bc_ind_list,
-            modify=args.modify_ratio,
-        )
-
-
-    if args.compare_ratio_method == 1:
-        print("gv.mean(gv_dataset['hp_o_pred']).shape:", gv.mean(ds_ratio_method['hp_o_pred']).shape)
         plt.errorbar(
-            x=np.arange(0, n_tau),
-            y=gv.mean(ds_ratio_method['hp_o_pred']),
-            yerr=gv.sdev(ds_ratio_method['hp_o_pred']),
-            c='c',
-            label='Pred Ratio Method',
+            x = np.arange(0, num_tau),
+            y = gv.mean(ds_ratio_method['hp_o_pred']),
+            yerr = gv.sdev(ds_ratio_method['hp_o_pred']),
+            c = 'c',
+            label = 'Pred Ratio Method',
         )
-
     if args.compare_ml_ratio_method == 1:
-        print("gv.mean(gv_dataset['hp_o_pred']).shape:", gv.mean(ds_ml_ratio_method['hp_o_pred']).shape)
         plt.errorbar(
-            x=np.arange(0, n_tau),
-            y=gv.mean(ds_ml_ratio_method['hp_o_pred']),
-            yerr=gv.sdev(ds_ml_ratio_method['hp_o_pred']),
-            c='m',
-            label='Pred ML+Ratio Method',
+            x = np.arange(0, num_tau),
+            y = gv.mean(ds_ml_ratio_method['hp_o_pred']),
+            yerr = gv.sdev(ds_ml_ratio_method['hp_o_pred']),
+            c = 'm',
+            label = 'Pred ML+Ratio Method',
         )
 
     plt.xlabel('Time extent')
@@ -146,32 +111,21 @@ def plot_correlators(*args) -> None:
 #===================================================================================================
 # RELATIVE CORRELATED DIFFERENCES 
 #===================================================================================================
-def relative_correlated_difference(
-    num_tau: int,
-    num_cfgs: int,
-    dict_data: dict[str, npt.NDArray],
-    dict_results: dict[str, npt.NDArray]
+def plot_relative_correlated_difference(
+    n_corr_o_unlab_vs_tau, 
+    n_corr_o_unlab_pred_vs_tau,
+    n_corr_o_bc_vs_tau,
+    n_corr_o_bc_pred_vs_tau
 ) -> None:
     """
     Computes and plots relative correlated differences for correlator data.
     
     Args:
-        corr_o_uncorrected: Normalized, bias-uncorrected correlator averaged over source times
-        corr_o_uncorrected: Normalized, bias-corrected correlator averaged over source times
+        num_tau: Number of time extents being used for fitting
+        num_cfgs: Number of configurations in the original dataset
+        dict_data: Dictionary of originally preprocessed correlator data
+        dict_results: Dictionary of resultant correlator data from inference
     """
-    n_corr_o_unlab_tensor = dict_data["n_corr_o_unlab_tensor"]
-    n_corr_o_bc_tensor = dict_data["n_corr_o_bc_tensor"]
-    
-    n_corr_o_unlab_pred_tensor = dict_results["n_corr_o_unlab_pred_tensor"]
-    n_corr_o_bc_pred_tensor = dict_results["n_corr_o_bc_pred_tensor"]
-
-    n_corr_o_unlab_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_unlab_tensor, num_tau, num_cfgs)
-    n_corr_o_unlab_pred_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_unlab_pred_tensor, num_tau, num_cfgs)
-
-    n_corr_o_bc_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_bc_tensor, num_tau, num_cfgs)
-    n_corr_o_bc_pred_vs_tau = tensor_to_avg_over_tsrc(n_corr_o_bc_pred_tensor, num_tau, num_cfgs)
-
-
     diff_n_corr_o_uncorrected = n_corr_o_unlab_pred_vs_tau - n_corr_o_unlab_vs_tau
     diff_n_corr_o_corrected = diff_n_corr_o_uncorrected + n_corr_o_bc_vs_tau - n_corr_o_bc_pred_vs_tau
 
@@ -196,9 +150,27 @@ def relative_correlated_difference(
 #===================================================================================================
 # NOISE TO SIGNAL RATIOS
 #===================================================================================================
-def noise_to_signal(*args) -> None:
+def plot_noise_to_signal(
+    num_tau: int,
+    corr_o_truth,
+    corr_o_labeled_truth,
+    corr_o_pred_corrected,
+    corr_o_pred_uncorrected,
+    ds_ratio_method,
+    ds_ml_ratio_method,
+    *args
+) -> None:
     """
     DOCS TODO
+
+    Args:
+        num_tau: Number of time extents used for fitting
+        corr_o_truth: Truth-level correlator data
+        corr_o_labeled_truth: Subset of truth data reserved for training and bias correction
+        corr_o_pred_corrected: Bias-corrected correlator predicted by the ML model
+        corr_o_pred_uncorrected: Bias-uncorrected correlator predicted by the ML model
+        ds_ratio_method: Dictionary of correlators output by ratio method
+        ds_ML_ratio_method: Dictionary of correlators output by ratio method combined with ML
     """
     fig = plt.figure(figsize=(8., 6.))
 
@@ -206,34 +178,29 @@ def noise_to_signal(*args) -> None:
     yscale = 'log'
 
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_truth, axis=-1)/np.average(corr_o_truth, axis=-1),
         c='b',
         linewidth=1.4,
         label='Truth',
     )
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_labeled_truth, axis=-1)/np.average(corr_o_labeled_truth, axis=-1),
         c='g',
         linewidth=1.3,
         label='Labeled set',
     )
-    # plt.plot(
-    #     np.arange(0, n_tau),
-    #     sem(corr_o_train_truth, axis=-1)/np.average(corr_o_train_truth, axis=-1),
-    #     c='violet',
-    #     label='Train set',
-    # )
+
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_pred_corrected, axis=-1)/np.average(corr_o_pred_corrected, axis=-1),
         c='r',
         linewidth=1.2,
         label='Pred Corrected',
     )
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_pred_uncorrected, axis=-1)/np.average(corr_o_pred_uncorrected, axis=-1),
         c='gold',
         linewidth=1.1,
@@ -242,7 +209,7 @@ def noise_to_signal(*args) -> None:
 
     if args.compare_ratio_method == 1:
         plt.plot(
-            np.arange(0, n_tau),
+            np.arange(0, num_tau),
             gv.sdev(ds_ratio_method['hp_o_pred']) / gv.mean(ds_ratio_method['hp_o_pred']),
             c='c',
             linewidth=1.0,
@@ -251,7 +218,7 @@ def noise_to_signal(*args) -> None:
 
     if args.compare_ml_ratio_method == 1:
         plt.plot(
-            np.arange(0, n_tau),
+            np.arange(0, num_tau),
             gv.sdev(ds_ml_ratio_method['hp_o_pred']) / gv.mean(ds_ml_ratio_method['hp_o_pred']),
             c='m',
             linewidth=0.9,
@@ -266,7 +233,16 @@ def noise_to_signal(*args) -> None:
     save_plot(fig=fig, path='./plots/', filename='pred_nts')
     
 
-def normalized_noise_to_signal(*args) -> None:
+def plot_normalized_noise_to_signal(
+    num_tau: int,
+    corr_o_truth,
+    corr_o_labeled_truth,
+    corr_o_pred_corrected,
+    corr_o_pred_uncorrected,
+    ds_ratio_method,
+    ds_ml_ratio_method,
+    *args
+) -> None:
     """
     Docs TODO
     """
@@ -274,7 +250,7 @@ def normalized_noise_to_signal(*args) -> None:
     fig = plt.figure(figsize=(8., 6.))
     normalization_denominator = sem(corr_o_truth, axis=-1)/np.average(corr_o_truth, axis=-1)
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_truth, axis=-1)/np.average(corr_o_truth, axis=-1)/normalization_denominator,
         c='b',
         linewidth=1.4,
@@ -282,7 +258,7 @@ def normalized_noise_to_signal(*args) -> None:
     )
 
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_labeled_truth, axis=-1)/np.average(corr_o_labeled_truth, axis=-1)/normalization_denominator,
         c='g',
         linewidth=1.3,
@@ -290,14 +266,14 @@ def normalized_noise_to_signal(*args) -> None:
     )
 
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_pred_corrected, axis=-1)/np.average(corr_o_pred_corrected, axis=-1)/normalization_denominator,
         c='r',
         linewidth=1.2,
         label='Pred Corrected',
     )
     plt.plot(
-        np.arange(0, n_tau),
+        np.arange(0, num_tau),
         sem(corr_o_pred_uncorrected, axis=-1)/np.average(corr_o_pred_uncorrected, axis=-1)/normalization_denominator,
         c='gold',
         linewidth=1.1,
@@ -306,7 +282,7 @@ def normalized_noise_to_signal(*args) -> None:
 
     if args.compare_ratio_method == 1:
         plt.plot(
-            np.arange(0, n_tau),
+            np.arange(0, num_tau),
             gv.sdev(ds_ratio_method['hp_o_pred']) / gv.mean(ds_ratio_method['hp_o_pred'])/normalization_denominator,
             c='c',
             linewidth=1.0,
@@ -315,7 +291,7 @@ def normalized_noise_to_signal(*args) -> None:
 
     if args.compare_ml_ratio_method == 1:
         plt.plot(
-            np.arange(0, n_tau),
+            np.arange(0, num_tau),
             gv.sdev(ds_ml_ratio_method['hp_o_pred']) / gv.mean(ds_ml_ratio_method['hp_o_pred'])/normalization_denominator,
             c='m',
             linewidth=0.9,
@@ -330,14 +306,19 @@ def normalized_noise_to_signal(*args) -> None:
     save_plot(fig=fig, path='./plots/', filename='pred_nts_normalized')
     
 
+#===================================================================================================
+# ERROR BREAKDOWN
+#===================================================================================================
 def plot_error_breakdown(
     pred_corrected,
     pred_uncorrected,
     bias_correction,
-    fig_name='./error_breakdown',
-    truth=None,
+    fig_name = './error_breakdown',
+    truth = None,
 ) -> None:
-
+    """
+    Docs TODO
+    """
     sem_pred_corrected = sem(pred_corrected, axis=-1)
     sem_pred_uncorrected = sem(pred_uncorrected, axis=-1)
     sem_bias_correction = sem(bias_correction, axis=-1)
