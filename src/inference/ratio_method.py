dccab6a37f29e2e5d731b1aa7d7233ca6f94a16c
ovega141 <vega00087@gmail.com>
2024-07-19 18:08:29 +0200

    implemented RatioMethod object

--- src/inference/ratio_method.py
+++ src/inference/ratio_method.py
@@ -12,23 +12,19 @@ from processing.preprocessing import convert_to_gvars
 
 class RatioMethod:
     """
-    Unboosted ratio method.
-
-    Args:
-        TODO
-    
-    Attributes:
-        TODO
+    Docs TODO
     """
     def __init__(
-        self, 
-        corr_2pt_i: npt.NDArray, 
-        corr_2pt_o: npt.NDArray, 
-        lab_ind_list: List[int]
+        self,
+        corr_i: npt.NDArray,
+        corr_o: npt.NDArray,
+        lab_ind_list: List[int],
+        boosted: bool,
+        alpha: Optional[npt.NDArray] = None
     ):
-        lp_i = corr_2pt_i[:, :, lab_ind_list]
-        lp_o = corr_2pt_o[:, :, lab_ind_list]
-        corrs: list[npt.NDArray] = [corr_2pt_i, corr_2pt_o, lp_i, lp_o]
+        lp_i = corr_i[:, :, lab_ind_list]
+        lp_o = corr_o[:, :, lab_ind_list]
+        corrs: list[npt.NDArray] = [corr_i, corr_o, lp_i, lp_o]
         
         for ix, corr in enumerate(corrs):
             corrs[ix] = np.average(corr, axis=-1)
@@ -40,24 +36,63 @@ class RatioMethod:
         
         self.gv_dataset = convert_to_gvars(dict_orig_corrs, averages_tsrc=False)
 
-    @staticmethod
-    def __predict(
-        X_lp: npt.NDArray, 
-        X_hp: npt.NDArray, 
-        Y_lp: npt.NDArray
-    ) -> npt.NDArray:
-        return Y_lp * X_hp / X_lp
-    
-    def predict(self):
-        X_lp = self.gv_dataset['lp_i']
-        X_hp = self.gv_dataset['hp_i']
-        Y_lp = self.gv_dataset['lp_o']
+        self.boosted = boosted
+        self.alpha = alpha
 
-        Y_hp = self.__predict(X_lp, X_hp, Y_lp)
-        self.gv_dataset['hp_o_pred'] =  Y_hp
+    def _truncate_alpha(
+        self, 
+        numerical_truncation: Optional[float] = 1e-50
+    ) -> None:
+        """
+        Numerically truncates the boost power `alpha`.
+        """
+        # Noise-to-Signal ratios
+        nts_hp_i = gv.sdev(self.gv_dataset['hp_i']) / gv.mean(self.gv_dataset['hp_i'])
+        nts_lp_i = gv.sdev(self.gv_dataset['lp_i']) / gv.mean(self.gv_dataset['lp_i'])
+        nts_lp_o = gv.sdev(self.gv_dataset['lp_o']) / gv.mean(self.gv_dataset['lp_o'])
+        
+        s = nts_hp_i / nts_lp_i
+        x = nts_lp_i
+        y = nts_lp_o
+
+        if self.alpha is None:
+            self.alpha = y / x / (1. + s**2)
+        gv_hp_i_alpha = self.gv_dataset['hp_i'] ** self.alpha
+        alpha_truncated = copy.deepcopy(self.alpha)
+
+        for j in range(gv_hp_i_alpha.shape[0]):
+            gv_hp_i_alpha_j_mean = gv.mean(gv_hp_i_alpha[j])
+            gv_hp_i_alpha_j_std = gv.sdev(gv_hp_i_alpha[j])
+            
+            five_sigma_lower = gv_hp_i_alpha_j_mean - 5 * gv_hp_i_alpha_j_std
+            five_sigma_higher = gv_hp_i_alpha_j_mean + 5 * gv_hp_i_alpha_j_std
+            is_inside_five_sigma: bool = (five_sigma_lower < 0 and five_sigma_higher > 0)
+            is_truncated: bool = (np.abs(gv_hp_i_alpha_j_mean) < numerical_truncation)
+
+            if is_inside_five_sigma or is_truncated or x[j] < 0 or y[j] < 0:
+                alpha_truncated[j] = 1.
+        self.alpha = alpha_truncated
+    
+    def fit(self) -> npt.NDArray:
+        """
+        Constructs the ratio from given data.
+        """
+        return self.gv_dataset['hp_i'] / self.gv_dataset['lp_i']
+        
+    def predict(self):
+        """
+        Computes the predicted output correlator using the ratio method estimator.
+        """
+        ratio = self.fit()
+        self.gv_dataset['hp_o_pred'] = self.gv_dataset['lp_o'] * ratio
+        if self.boosted:
+            boosted_ratio = (self.gv_dataset['hp_i'] / self.gv_dataset['lp_i']) ** self.alpha
+            boosted_ratio = np.where(self.alpha != None, boosted_ratio, ratio)
+            self.gv_dataset['hp_o_pred_modified'] = self.gv_dataset['lp_o'] * boosted_ratio
         return self.gv_dataset
 
 
+#===================================================================================================
 def ratio_method(
     corr_2pt_i: npt.NDArray, 
     corr_2pt_o: npt.NDArray, 
