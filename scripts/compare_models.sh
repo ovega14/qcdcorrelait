d9b53ed29b665d18f160b5fd0ed04708248131b8
ovega141 <vega00087@gmail.com>
2024-07-29 17:20:33 +0100

    edited but didnt test .sh script to loop over several ML models

--- scripts/compare_models.sh
+++ scripts/compare_models.sh
@@ -27,36 +27,40 @@ mo2label=("0.01555")
 params=()
 for i in "${!mi1label[@]}"
 do
-    param=("$use_torch $reg_method0 P5-P5_RW_RW_d_d_m"${mi1label[$i]}"_m"${mi2label[$i]}"_p000 P5-P5_RW_RW_d_d_m"${mo1label[$i]}"_m"${mo2label[$i]}"_p000 {\"lr\":0.01,\"l2_coeff\":1e-2,\"training_steps\":200}")
-    read use_torch reg_method input_dataname output_dataname dict_hyperparams<<< "$param"
-    echo $use_torch $reg_method $input_dataname $output_dataname $dict_hyperparams
-    NOW=$(date +"%Y-%m-%d-%H-%M-%S")
-    num=$num
-    name=test_model_$reg_method+mi1_${mi1label[$i]}_mi2_${mi2label[$i]}_mo1_${mo1label[$i]}_mo2_${mo2label[$i]}+$NOW+$num
-    if [ -d "results/"$name ]
-    then
-        echo "results/"$name" already exists"
-        read -n 1 -p "keep running? [y/n]" prompt
-        if [[ $prompt == "y" || $prompt == "yes" || $prompt == "Yes" ]]
+
+    for reg_method in reg_methods
+    do
+        param=("$use_torch $reg_method0 P5-P5_RW_RW_d_d_m"${mi1label[$i]}"_m"${mi2label[$i]}"_p000 P5-P5_RW_RW_d_d_m"${mo1label[$i]}"_m"${mo2label[$i]}"_p000 {\"lr\":0.01,\"l2_coeff\":1e-2,\"training_steps\":200}")
+        read use_torch reg_method input_dataname output_dataname dict_hyperparams<<< "$param"
+        echo $use_torch $reg_method $input_dataname $output_dataname $dict_hyperparams
+        NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+        num=$num
+        name=test_model_$reg_method+mi1_${mi1label[$i]}_mi2_${mi2label[$i]}_mo1_${mo1label[$i]}_mo2_${mo2label[$i]}+$NOW+$num
+        if [ -d "results/"$name ]
         then
-            echo "proceed"
-        else
-            echo -e "\nexit"
-            exit
+            echo "results/"$name" already exists"
+            read -n 1 -p "keep running? [y/n]" prompt
+            if [[ $prompt == "y" || $prompt == "yes" || $prompt == "Yes" ]]
+            then
+                echo "proceed"
+            else
+                echo -e "\nexit"
+                exit
+            fi
         fi
-    fi
-    mkdir -p ../results/$name
-    mkdir ../results/$name/data
-    mkdir ../results/$name/plots
-    mkdir ../results/$name/model
-    mkdir ../results/$name/results
-		python -W ignore main.py \
-        --seed $seed \
-        --use_torch $use_torch \
-        --input_dataname $input_dataname \
-        --output_dataname $output_dataname \
-        --reg_method $reg_method \
-        --dict_hyperparams $dict_hyperparams \
-        --rel_eps $rel_eps \
-        --results_dir "../results/$name"
+        mkdir -p ../results/$name
+        mkdir ../results/$name/data
+        mkdir ../results/$name/plots
+        mkdir ../results/$name/model
+        mkdir ../results/$name/results
+            python -W ignore main.py \
+            --seed $seed \
+            --use_torch $use_torch \
+            --input_dataname $input_dataname \
+            --output_dataname $output_dataname \
+            --reg_method $reg_method \
+            --dict_hyperparams $dict_hyperparams \
+            --rel_eps $rel_eps \
+            --results_dir "../results/$name"
+    done
 done
