5f1d7cbb6c530823cd6936e12245b51783b93dd5
ovega141 <vega00087@gmail.com>
2024-07-30 14:31:07 +0100

    changed .sh script params

--- scripts/compare_models.sh
+++ scripts/compare_models.sh
@@ -9,8 +9,7 @@ rel_eps=1e-2
 # respecify params here !!!
 #torch_reg_methods=("Linear" "MLP" "CNN" "Transformer" )
 #sklearn_reg_methods=("DTR" "RFR" "GBR" "LinearRegression" "Ridge")
-#reg_methods=("MLP" "CNN" "Transformer" "Ridge" "GBR")
-reg_methods=("MLP" "Ridge")
+reg_methods=("MLP" "CNN" "Transformer" "Ridge" "GBR")
 
 mi1label=("0.548")
 mi2label=("0.01555")
