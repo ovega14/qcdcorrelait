f55fc737c13de82d22d18c8f9612b2c2d9e9a554
ovega141 <vega00087@gmail.com>
2024-09-23 15:43:41 -0500

    removed unnecessary arguments from fit_truth.sh script to make it more readable

--- scripts/fit_truth.sh
+++ scripts/fit_truth.sh
@@ -5,28 +5,22 @@ export OMP_NUM_THREADS=1
 seed=42
 num=0
 
-mi1label=("0.548")
-mi2label=("0.01555")
 mo1label=("0.164")
 mo2label=("0.01555")
-# mi1label=("0.164" "0.1827" "0.365" "0.548" "0.731" "0.843")
-# mi2label=("0.01555" "0.01555" "0.01555" "0.01555" "0.01555" "0.01555")
 # mo1label=("0.164" "0.1827" "0.365" "0.548" "0.731" "0.843")
 # mo2label=("0.00311" "0.00311" "0.00311" "0.00311" "0.00311" "0.00311")
-# mi1label=("0.164" "0.164" "0.164" "0.164" "0.164" "0.164")
-# mi2label=("0.01555" "0.01555" "0.01555" "0.01555" "0.01555" "0.01555")
 # mo1label=("0.164" "0.1827" "0.365" "0.548" "0.731" "0.843")
 # mo2label=("0.01555" "0.01555" "0.01555" "0.01555" "0.01555" "0.01555")
 
 params=()
-for i in "${!mi1label[@]}"
+for i in "${!mo1label[@]}"
 do
-    param=("P5-P5_RW_RW_d_d_m"${mi1label[$i]}"_m"${mi2label[$i]}"_p000 P5-P5_RW_RW_d_d_m"${mo1label[$i]}"_m"${mo2label[$i]}"_p000")
-    read input_dataname output_dataname <<< "$param"
-    echo TRUTH_$input_dataname $output_dataname
+    param=("P5-P5_RW_RW_d_d_m"${mo1label[$i]}"_m"${mo2label[$i]}"_p000")
+    read output_dataname <<< "$param"
+    echo TRUTH_$output_dataname
     NOW=$(date +"%Y-%m-%d-%H-%M-%S")
     num=$num
-    name=fit_TRUTH_mi1_${mi1label[$i]}_mi2_${mi2label[$i]}_mo1_${mo1label[$i]}_mo2_${mo2label[$i]}+$NOW+$num
+    name=fit_TRUTH_mo1_${mo1label[$i]}_mo2_${mo2label[$i]}+$NOW+$num
     if [ -d "results/"$name ]
     then
         echo "results/"$name" already exists"
@@ -45,7 +39,6 @@ do
     mkdir ../results/$name/results
     python -W ignore ../frontend/fit_corrs.py \
         --seed $seed \
-        --input_dataname $input_dataname \
         --output_dataname $output_dataname \
         --results_dir "../results/$name"
     rm ../results/$name/*.pkl
